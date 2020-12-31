e2c12e10778298f7c6e1f21bd8b6f47ef143abaf
Kyle Pericak <kyle@pericak.com>
2020-12-31 07:52:02 -0500

    Updated setup.py release settings for 1.01

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="f-encrypt",
-    version="1.0",
+    version="1.01",
+    license='',
     include_package_data=True,
     packages=["fencrypt"],
     install_requires=["Click", "pycryptodome"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Kyle Pericak',
+    author_email='kyle@pericak.com',
     url="https://github.com/kylep/f-encrypt",
-    download_url="https://github.com/kylep/f-encrypt/archive/1.0.tar.gz",
+    download_url="https://github.com/kylep/f-encrypt/archive/1.01.tar.gz",
     classifiers=["Topic :: Security :: Cryptography"],
     entry_points={
         "console_scripts": [
